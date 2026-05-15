0809d8ebad3007373e5b124e5dd7aa0affbd4be0
burningclasher20-bit <burningclasher.20@gmail.com>
2026-05-15 23:21:26 +0530

    CHANGED NEXT CONFIG

--- NIFT/next.config.mjs
+++ NIFT/next.config.mjs
@@ -2,6 +2,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
+  typescript: {
+    ignoreBuildErrors: true
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
