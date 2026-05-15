5c0e6211191fcc95ce7d4bd9065c3453150e47c6
burningclasher20-bit <burningclasher.20@gmail.com>
2026-05-15 23:19:12 +0530

    changed next config

--- NIFT/next.config.mjs
+++ NIFT/next.config.mjs
@@ -1,14 +1,11 @@
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com'
-      }
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' }
     ]
   }
 };
